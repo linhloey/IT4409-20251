4bc73cf0d0728fc9f39a395bd23c2860a6eddd3e
Lamame <hoanglam572003@gmail.com>
2025-12-16 12:20:50 +0700

    create API

--- server/src/routes/UserRouter.js
+++ server/src/routes/UserRouter.js
@@ -1,5 +1,12 @@
 const express = require("express");  
 const router = express. Router()  
 const userController = require('../controllers/UserController')  
-router.post('/', userController.createUser)  
+
+router.post('/sign-up', userController.createUser)
+router.post('/sign-in', userController.loginUser)
+router.put('/update-user/:id', userController.updateUser)
+router.delete('/delete-user/:id',authMiddleWare ,userController.deleteUser)
+router.get('/getALL',authMiddleWare ,userController.getAllUser)
+router.get('/get-details/:id' ,userController.getDetailsUser)
+
 module.exports = router  
